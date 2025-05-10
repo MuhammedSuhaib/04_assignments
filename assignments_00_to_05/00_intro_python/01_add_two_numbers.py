2889ac827882f153f0908987ef02a93af8930e38
Suhaib Umair <154515857+MuhammedSuhaib@users.noreply.github.com>
2025-05-10 21:13:53 +0500

    sum of 2 nums

---
+++ assignments_00_to_05/00_intro_python/01_add_two_numbers.py
@@ -0,0 +1,14 @@
+def main():
+    """
+    Program: add2numbers
+    --------------------
+    This program asks the user for two
+    integers and prints their sum.
+    """
+    print("This program adds two numbers.")
+    num1 : int = int(input("Enter first number: "))
+    num2 : int = int(input("Enter second number: "))
+    total: int = num1 + num2
+    print(f'The total is {total} .')
+if __name__ == '__main__':
+    main()
