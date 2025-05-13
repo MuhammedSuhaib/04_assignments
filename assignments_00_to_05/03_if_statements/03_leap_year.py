d36b9516407f129b5d7b544a946b95999b0c4b91
Suhaib Umair <154515857+MuhammedSuhaib@users.noreply.github.com>
2025-05-13 19:35:45 +0500

    Add leap year determination functionality

---
+++ assignments_00_to_05/03_if_statements/03_leap_year.py
@@ -0,0 +1,8 @@
+def main():
+    year = int(input('Please input a year: '))
+    is_leap = year % 4 == 0 and (year % 100 != 0 or year % 400 == 0)
+    print("That's a leap year!" if is_leap else "That's not a leap year.")
+
+
+if __name__ == '__main__':
+    main()
