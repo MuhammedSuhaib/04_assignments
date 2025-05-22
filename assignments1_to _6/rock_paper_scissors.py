613c73f0aa60800cac83aa8e46f665eee4c307b6
Suhaib Umair <154515857+MuhammedSuhaib@users.noreply.github.com>
2025-05-22 23:40:09 +0500

    Implement rock-paper-scissors game with user input and computer choice

---
+++ assignments1_to _6/rock_paper_scissors.py
@@ -0,0 +1,20 @@
+import random
+
+choices = {"r": "rock", "p": "paper", "s": "scissors"}
+user = input("Choose rock = r, paper = p, or scissors = s: ").lower()
+
+if user not in choices:
+    print("Invalid choice. Please Choose rock = r, paper = p, or scissors = s:.")
+else:
+    computer = random.choice(list(choices.values()))
+    user_choice = choices[user]
+    print(f"You: {user_choice} | Computer: {computer}")
+
+    if user_choice == computer:
+        print("It's a tie!")
+    elif (user_choice == "rock" and computer == "scissors") or \
+         (user_choice == "paper" and computer == "rock") or \
+         (user_choice == "scissors" and computer == "paper"):
+        print("You win!🎉")
+    else:
+        print("You lose!😢")
